9d1a5ec239cbb0468ed13d55befd4685012073ef
Eduardo <edumoraesdg@gmail.com>
2022-12-31 17:06:19 -0400

    Exemplo 6.1

---
+++ src/Exemplo_6_1.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+
+public class Exemplo_6_1 {
+    public static void main(String[] args) {
+        try{
+            float[] vetor = new float[10];
+            float media, soma = 0;
+            String num;
+            for (int i = 0; i < vetor.length; i++) {
+                num = JOptionPane.showInputDialog(
+                        "Digite o valor " + i + ":");
+                vetor[i] = Float.parseFloat(num);
+                soma = soma + vetor[i];
+            }
+            media = soma / vetor.length;
+            JOptionPane.showMessageDialog(
+                    null, "Média: " + media);
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(
+                    null, "Ocorreu um erro durante a leitura!");
+        }
+    }
+}
