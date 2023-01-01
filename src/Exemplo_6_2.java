abf296280927e966848e3dae599e2636d6913bf3
Eduardo <edumoraesdg@gmail.com>
2023-01-01 16:22:18 -0400

    Comprimento do vetor definido pelo usuário

--- src/Exemplo_6_2.java
+++ src/Exemplo_6_2.java
@@ -2,23 +2,28 @@ import javax.swing.*;
 
 public class Exemplo_6_2 {
     public static void main(String[] args) {
-        int[] teste1 = new int[10];
-        int[] teste2 = new int[10];
+        String vetor = JOptionPane.showInputDialog(
+                "informe o tamanho do vetor"
+        );
+        int tamanhoVetor = Integer.parseInt(vetor);
+        int[] teste1 = new int[tamanhoVetor];
+        int[] teste2 = new int[tamanhoVetor];
         String num;
         try{
-            for(int i = 0; i < teste1.length; i++) {
+
+            for(int i = 0; i < tamanhoVetor; i++) {
                 num = JOptionPane.showInputDialog(
                         "Digite o valor " + i + ":");
                 teste1[i] = Integer.parseInt(num);
             }
-            for(int i = 0; i < teste1.length; i++) {
+            for(int i = 0; i < tamanhoVetor; i++) {
                 if(i % 2 == 0)
                     teste2[i] = teste1[i] * 5;
                 else
                     teste2[i] = teste1[i] + 5;
             }
             System.out.printf("%s/n", "Resultado:");
-            for(int i = 0; i < 10; i++) {
+            for(int i = 0; i < tamanhoVetor; i++) {
                 System.out.printf("%s%d%s%d",
                         "teste1[", i, "]= ", teste1[i]);
                 System.out.printf("%10s%d%s%d\n",
