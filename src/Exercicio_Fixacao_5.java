ce4e68117e57c3bd670656521e9a663731f4f47b
Eduardo <edumoraesdg@gmail.com>
2023-01-03 20:21:27 -0400

    Exercício de Fixação 05

---
+++ src/Exercicio_Fixacao_5.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+
+public class Exercicio_Fixacao_5 {
+    public static void main(String[] args) {
+        final int diasSemana = 7;
+        float[] temperatura = new float[diasSemana];
+        temperatura[0] = 19.0f;
+        temperatura[1] = 23.0f;
+        temperatura[2] = 21.0f;
+        temperatura[3] = 25.0f;
+        temperatura[4] = 22.0f;
+        temperatura[5] = 20.0f;
+        temperatura[6] = 24.0f;
+        float x;
+        for(int i = 0; i < diasSemana; i++) {
+            for(int j = i + 1; j < diasSemana; j++) {
+                if(temperatura[j] > temperatura[i]) {
+                    x = temperatura[i];
+                    temperatura[i] = temperatura[j];
+                    temperatura[j] = x;
+                }
+            }
+        }
+        JOptionPane.showMessageDialog(
+                null, "Mínima da semana = " + temperatura[6]);
+        JOptionPane.showMessageDialog(
+                null, "Máxima da semana = " + temperatura[0]);
+    }
+}
