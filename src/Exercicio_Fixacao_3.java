7b33eeb264926ca95ad8930954983d72c0f61c82
Eduardo <edumoraesdg@gmail.com>
2023-01-03 20:12:30 -0400

    Exercício de Fixação 03

---
+++ src/Exercicio_Fixacao_3.java
@@ -0,0 +1,7 @@
+public class Exercicio_Fixacao_3 {
+    public static void main(String[] args) {
+        char[] vogais = new char[5];
+        float[] altura = new float[10];
+        String[] meses = new String[12];
+    }
+}
